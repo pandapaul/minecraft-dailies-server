add15722a44b7a8e4d667d48c1a9eb19798eed4d
Paul Anderson <thesamuraipanda@gmail.com>
2016-04-10 13:56:18 -0400

    adding some random quest generation. these should probably be switched to just picking from a decent-sized set of hand-written quests. that way things will be consistent and fair.  random is fine for now and can be improved upon, of course.

--- routes/quests.js
+++ routes/quests.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var chance = require('chance').Chance();
 
 router.get('/', function (req, res) {
     var quests = generateQuests();
@@ -13,11 +14,74 @@ function generateQuests() {
         numberOfQuestsToGenerate--;
         quests.push(generateQuest());
     }
+    return quests;
 }
 
 function generateQuest() {
+    var type = questType();
+    var target = questTarget(type);
+    var reward = questReward();
+
     return {
-        
+        type: type,
+        target: target,
+        reward: reward
+    };
+}
+
+function questType() {
+    return chance.pickone([
+        'hunt',
+        'gather'
+    ]);
+}
+
+function questTarget(type) {
+    var target = {};
+    if (type === 'hunt') {
+        target.type = chance.pickone([
+            'zombie',
+            'skeleton',
+            'creeper',
+            'spider'
+        ]);
+        target.quantity = 10;
+    } else if (type === 'gather') {
+        target = chance.pickone([
+            {
+                type: 'iron',
+                quantity: 20
+            },
+            {
+                type: 'gold',
+                quantity: 15
+            },
+            {
+                type: 'diamond',
+                quantity: 5
+            },
+            {
+                type: 'redstone',
+                quantity: 30
+            },
+            {
+                type: 'emerald',
+                quantity: 5
+            }
+        ]);
+    }
+
+    return target;
+}
+
+function questReward() {
+    var multiplier = chance.normal({
+        mean: 1,
+        dev: 0.15
+    });
+    return {
+        type: 'xp',
+        quantity: Math.round((multiplier * 5000))
     };
 }
 
