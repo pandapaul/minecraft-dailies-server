02bc76a45ad953bbdd5055881d6d5ece30ab0c41
Paul Anderson <panderson@add123.com>
2016-05-04 11:24:56 -0400

    require username in auth

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,9 @@ function authenticate(req) {
     if (!req.body.accessToken) {
         return Promise.reject('No accessToken provided.');
     }
+    if (!req.body.username) {
+        return Promise.reject('No username provided.');
+    }
     return new Promise(function (resolve,reject) {
         yggdrasil.validate(req.body.accessToken, function (isValid, err) {
             if (err) {
