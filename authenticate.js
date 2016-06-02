60f282ef654f4e2100a1c91145120a38c52f26b1
Paul Anderson <thesamuraipanda@gmail.com>
2016-06-02 07:49:06 -0400

    accept username on body or params of request

--- authenticate.js
+++ authenticate.js
@@ -4,7 +4,7 @@ function authenticate(req) {
     if (!req.body.accessToken) {
         return Promise.reject('No accessToken provided.');
     }
-    if (!req.body.username) {
+    if (!req.body.username && !req.params.username) {
         return Promise.reject('No username provided.');
     }
     return new Promise(function (resolve,reject) {
