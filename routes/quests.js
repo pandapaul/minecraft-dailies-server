258b5b21e348b0372a178ebbd0d787a2e43922cf
Paul Anderson <thesamuraipanda@gmail.com>
2016-06-01 23:09:48 -0400

    moving some quests stuff into users router

--- routes/quests.js
+++ routes/quests.js
@@ -20,47 +20,4 @@ router.get('/:questId', function (req, res) {
         });
 });
 
-router.use('/:questId', function (req, res, next) {
-    authenticate(req)
-        .then(function (data) {
-            return validateQuestId(req.params.questId);
-        })
-        .then(function (questIsValid) {
-            if (questIsValid) {
-                next();
-            } else {
-                return Promise.reject('Invalid quest ID');
-            }
-        })
-        .catch(function (err) {
-            next(err);
-        });
-});
-
-function validateQuestId(questId) {
-    return questFetcher.fetchQuestById(questId)
-        .then(function (quest) {
-            if (quest && quest.id === questId) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-        .catch(function () {
-            return false;
-        });
-}
-
-router.post('/:questId/accept', function (req, res) {
-    res.end();
-});
-
-router.post('/:questId/complete', function (req, res) {
-    res.end();
-});
-
-router.post('/:questId/abandon', function (req, res) {
-    res.end();
-});
-
 module.exports = router;
